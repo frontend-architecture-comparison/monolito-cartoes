29f73cb6ddaa656ae424adcd5fa7b3699202de82
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2026-03-30 22:35:53 -0300

    feat: criando estrutura de modules do projeto

---
+++ src/app/features/cartoes/cartoes.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cartoes',
+  imports: [],
+  templateUrl: './cartoes.html',
+  styleUrl: './cartoes.scss',
+})
+export class Cartoes {}
